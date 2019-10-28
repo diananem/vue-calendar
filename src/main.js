cac2eaf1b9002598c271aa9ff9a4614126d3dba4
Diana Nemyrovska <diana.nemyrovska@gmail.com>
2019-10-28 20:04:41 +0200

    Initialize firebase

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import VueTextareaAutosize from "vue-textarea-autosize";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+import firebaseConfig from "./utils";
+
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false;
+
+firebase.initializeApp(firebaseConfig);
+
+new Vue({
+  vuetify,
+  render: h => h(App)
+}).$mount("#app");
